2af7731c638bd32a6c716191af3a84884595d728
amber <amber@amberdemacbook-pro.local>
2015-11-08 16:38:51 +0800

    Initial Commit

---
+++ Community/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Community
+//
+//  Created by amber on 15/11/8.
+//  Copyright © 2015年 shlity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Community/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Community
+//
+//  Created by amber on 15/11/8.
+//  Copyright © 2015年 shlity. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
